8a275feb424c0ff6477ca532a25798bb80732598
z001nz1 <saisubramanyam.purruchinnappa@target.com>
2020-05-04 13:00:49 -0500

    added graphs

--- src/com/datastructures/queues/impl/ArrayQueue.java
+++ src/com/datastructures/queues/impl/ArrayQueue.java
@@ -1,48 +1,49 @@
 package com.datastructures.queues.impl;
 
- class ArrayQueue {
+public class ArrayQueue {
 
     int front = 0;
     int rear = -1;
     int[] queueArray;
     int size;
 
-    ArrayQueue(int size){
+    public ArrayQueue(int size) {
         queueArray = new int[size];
         this.size = size;
     }
 
-    boolean isFull(){
-        return size-1 == rear;
+    boolean isFull() {
+        return size - 1 == rear;
     }
 
-    boolean isEmpty(){
+    public boolean isEmpty() {
         return rear == -1;
     }
 
-    void enqueue(int val){
-        if(!isFull()){
+    public void enqueue(int val) {
+        if (!isFull()) {
             queueArray[++rear] = val;
-        } else{
+        } else {
             System.out.println("Queue is full.");
         }
     }
 
-    int dequeue(){
-        if(!isEmpty()){
-            for(int i=0; i<rear; i++){
-                queueArray[i] = queueArray[i+1];
+    public int dequeue() {
+        int dequeuedElement = queueArray[front];
+        if (!isEmpty()) {
+            for (int i = 0; i < rear; i++) {
+                queueArray[i] = queueArray[i + 1];
             }
             rear--;
-            return queueArray[front];
-        } else{
+            return dequeuedElement;
+        } else {
             return -1;
         }
     }
 
-    void print(){
-        for(int n: queueArray){
-            System.out.print(n+" ");
+    void print() {
+        for (int n : queueArray) {
+            System.out.print(n + " ");
         }
     }
 }
