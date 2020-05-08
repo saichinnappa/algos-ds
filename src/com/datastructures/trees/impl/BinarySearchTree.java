f2683d4c48f0389fb7c0b757a5780b14b8f4e852
z001nz1 <saisubramanyam.purruchinnappa@target.com>
2020-05-08 09:59:06 -0500

    added more functionalities

--- src/com/datastructures/trees/impl/BinarySearchTree.java
+++ src/com/datastructures/trees/impl/BinarySearchTree.java
@@ -3,6 +3,36 @@ package com.datastructures.trees.impl;
 public class BinarySearchTree {
     Node root;
 
+    /**
+     * Pre traverse - ROOT -> LEFT -> RIGHT
+     */
+    public void preTraverse(Node root) {
+        if (root == null) return;
+        System.out.print(root.data + ",");
+        preTraverse(root.leftChild);
+        preTraverse(root.rightChild);
+    }
+
+    /**
+     * In traverse - LEFT -> ROOT -> RIGHT
+     */
+    public void inTraverse(Node root) {
+        if (root == null) return;
+        inTraverse(root.leftChild);
+        System.out.print(root.data + ",");
+        inTraverse(root.rightChild);
+    }
+
+    /**
+     * Post Traverse - LEFT -> RIGHT -> ROOT
+     */
+    public void postTraverse(Node root) {
+        if (root == null) return;
+        postTraverse(root.leftChild);
+        postTraverse(root.rightChild);
+        System.out.print(root.data + ",");
+    }
+
     boolean add(int val) {
         Node currentNode = this.root;
         if (currentNode == null) {
@@ -31,6 +61,11 @@ public class BinarySearchTree {
         return false;
     }
 
+    public boolean addRecursive(int value) {
+        root = recursive(this.root, value);
+        return true;
+    }
+
     Node recursive(Node currentNode, int val) {
         if (currentNode == null) {
             return new Node(val);
@@ -43,10 +78,77 @@ public class BinarySearchTree {
         return currentNode;
     }
 
-    public boolean addRecursive(int value) {
+    boolean search(int val) {
+        Node currentNode = this.root;
+        if (currentNode == null) {
+            System.out.println("Tree is empty!");
+        } else if (currentNode.data == val) {
+            return true;
+        }
+        while (currentNode != null) {
+            if (val == currentNode.data) {
+                return true;
+            }
+            if (val < currentNode.data) {
+                currentNode = currentNode.leftChild;
+            } else {
+                currentNode = currentNode.rightChild;
+            }
 
-        root = recursive(this.root, value);
-        return true;
+        }
+        return false;
+    }
+
+    int findMin(Node root) {
+        int minValue = Integer.MAX_VALUE;
+        if (root == null) {
+            System.out.println("There is no tree.");
+        } else {
+            minValue = root.data;
+        }
+        while (root != null) {
+            Node leftChild = root.leftChild;
+            if (leftChild != null && minValue >= leftChild.data) {
+                minValue = leftChild.data;
+            }
+            root = leftChild;
+        }
+        return minValue;
+    }
+
+    int findMinRecursive(Node root) {
+        if (root == null) {
+            return -1;
+        } else if (root.leftChild == null) {
+            return root.data;
+        } else {
+            return findMinRecursive(root.leftChild); //keep traversing left child until the leaf node, the left most leaf node will be having minimum value
+        }
+    }
+
+    boolean delete(int val) {
+        Node currentNode = this.root;
+        if (currentNode == null) {
+            System.out.println("Tree is empty");
+        }
+        while (currentNode != null) {
+            Node leftChild = currentNode.leftChild;
+            Node rightChild = currentNode.rightChild;
+            if (leftChild != null && val == leftChild.data) {
+                currentNode.leftChild = null;
+                return true;
+            }
+            if (rightChild != null && val == rightChild.data) {
+                currentNode.rightChild = null;
+                return true;
+            }
+            if (val < currentNode.data) {
+                currentNode = currentNode.leftChild;
+            } else if (val > currentNode.data) {
+                currentNode = currentNode.leftChild;
+            }
+        }
+        return false;
     }
 
     void print(Node node) {
--- src/com/datastructures/trees/impl/Main.java
+++ src/com/datastructures/trees/impl/Main.java
@@ -3,18 +3,35 @@ package com.datastructures.trees.impl;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
-        binarySearchTree.add(2);
-        binarySearchTree.add(2);
-        binarySearchTree.add(3);
-        binarySearchTree.add(4);
-        binarySearchTree.add(5);
-        binarySearchTree.print(binarySearchTree.root);
-        binarySearchTree.addRecursive(3);
-        binarySearchTree.addRecursive(0);
-        binarySearchTree.addRecursive(-1);
+//        binarySearchTree.add(2);
+//        binarySearchTree.add(2);
+//        binarySearchTree.add(3);
+//        binarySearchTree.add(4);
+//        binarySearchTree.add(5);
+//        binarySearchTree.print(binarySearchTree.root);
+        binarySearchTree.addRecursive(50);
+        binarySearchTree.addRecursive(20);
+        binarySearchTree.addRecursive(10);
+        binarySearchTree.addRecursive(70);
+        binarySearchTree.addRecursive(40);
+        binarySearchTree.addRecursive(90);
+        binarySearchTree.addRecursive(60);
         binarySearchTree.addRecursive(5);
-        binarySearchTree.addRecursive(-3);
-        binarySearchTree.addRecursive(6);
+        binarySearchTree.addRecursive(0);
+//        System.out.println("PRE-TRAVERSAL: ");
+//        binarySearchTree.preTraverse(binarySearchTree.root);
+//        System.out.println("\nIN-TRAVERSAL: ");
+//        binarySearchTree.inTraverse(binarySearchTree.root);
+//        System.out.println("\nPOST-TRAVERSAL: ");
+//        binarySearchTree.postTraverse(binarySearchTree.root);
+//        System.out.println("##### BEFORE DELETION #####");
+//        System.out.println("Root -> " + binarySearchTree.root.data);
         binarySearchTree.print(binarySearchTree.root);
+        System.out.println("MIN-VALUE: " + binarySearchTree.findMinRecursive(binarySearchTree.root));
+//        binarySearchTree.delete(70);
+//        System.out.println("##### AFTER DELETION #####");
+//        System.out.println("Root -> " + binarySearchTree.root.data);
+//        binarySearchTree.print(binarySearchTree.root);
+//        System.out.println(binarySearchTree.search(30));
     }
 }
