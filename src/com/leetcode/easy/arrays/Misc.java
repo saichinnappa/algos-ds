509c866a95e61f8f809f64e3de896a341a9d3867
z001nz1 <saisubramanyam.purruchinnappa@target.com>
2020-05-22 06:57:01 -0500

    more problems

---
+++ src/com/leetcode/easy/arrays/Misc.java
@@ -0,0 +1,106 @@
+package com.leetcode.easy.arrays;
+
+import java.util.*;
+
+public class Misc {
+    public static void main(String[] args) {
+//        int[] arr1 = {1, 2, 3, 4, 5};
+//        int[] arr2 = {1, 2, 5, 7, 9};
+//        int[] arr3 = {1, 3, 4, 5, 8};
+//        intersection3Arrays(arr1, arr2, arr3);
+//        int[] arr = {2, 1, 2, 1, 1, 2, 2, 1};
+//        transformArray(arr);
+
+
+    }
+
+    static List<Integer> intersection3Arrays(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> result = new ArrayList<Integer>();
+        Map<Integer, Integer> commonMap = new HashMap<>();
+
+        for (int n : arr1) {
+            commonMap.put(n, 1);
+        }
+        for (int i : arr2) {
+            if (commonMap.containsKey(i)) {
+                commonMap.put(i, commonMap.get(i) + 1);
+            }
+        }
+        for (int i : arr3) {
+            if (commonMap.containsKey(i)) {
+                commonMap.put(i, commonMap.get(i) + 1);
+            }
+        }
+        for (int n : commonMap.keySet()) {
+            if (commonMap.get(n) == 3) {
+                result.add(n);
+            }
+        }
+        Collections.sort(result);
+        return result;
+    }
+
+    /**
+     * Given an initial array arr, every day you produce a new array using the array of the previous day.
+     * <p>
+     * On the i-th day, you do the following operations on the array of day i-1 to produce the array of day i:
+     * <p>
+     * If an element is smaller than both its left neighbor and its right neighbor, then this element is incremented.
+     * If an element is bigger than both its left neighbor and its right neighbor, then this element is decremented.
+     * The first and last elements never change.
+     * After some days, the array does not change. Return that final array.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: arr = [6,2,3,4]
+     * Output: [6,3,3,4]
+     * Explanation:
+     * On the first day, the array is changed from [6,2,3,4] to [6,3,3,4].
+     * No more operations can be done to this array.
+     */
+
+    static List<Integer> transformArray(int[] arr) {
+        int[] res = Arrays.copyOf(arr, arr.length);
+        int flag = 0;
+        while (true) {
+            flag = 0;
+            for (int i = 1; i < arr.length - 1; i++) {
+                if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1]) {
+                    res[i] = arr[i] + 1;
+                    flag = 1;
+                } else if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                    System.out.println("index: " + i + " || i-1: " + arr[i - 1] + " || i: " + arr[i] + "|| i+1: " + arr[i + 1]);
+                    res[i] = arr[i] - 1;
+                    flag = 1;
+                }
+                System.out.println(Arrays.toString(res));
+            }
+
+            if (flag == 0) {
+                break;
+            } else {
+                System.out.println("---");
+                arr = Arrays.copyOf(res, res.length);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int num : res) {
+            list.add(num);
+        }
+        return list;
+    }
+
+    static void rotateRight(int[] nums, int k) {
+        while (k != 0) {
+            int temp = nums[nums.length - 1];
+            for (int i = nums.length - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = temp;
+            System.out.println(Arrays.toString(nums));
+            k--;
+        }
+    }
+
+
+}
